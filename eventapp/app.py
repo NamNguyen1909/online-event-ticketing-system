d565e94fe15366fdaea9e939b606d609b88d83c0
Nguyễn Phong Phú <npphus@gmail.com>
2025-07-22 12:14:31 +0700

    Update app.py

--- eventapp/app.py
+++ eventapp/app.py
@@ -1,8 +1,4 @@
 from eventapp import app, db
-from eventapp.auth import auth_bp, login_manager
-
-# Đăng ký blueprint
-app.register_blueprint(auth_bp, url_prefix='/auth')
 
 if __name__ == '__main__':
     with app.app_context():
